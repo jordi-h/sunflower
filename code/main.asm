69d8b84f8f4f9c7082f137905c9a203c43f27cff
Jordi <jordi.hoorelbeke@student.ulg.ac.be>
2022-10-15 10:19:57 +0200

    Code done (added some comments + rearange).

--- code/main.asm
+++ code/main.asm
@@ -6,27 +6,26 @@
  * 
  */
 
-PROCESSOR 16F1789 ; used processor definition
+PROCESSOR 16F1789       ; used processor definition
 
 #include <xc.inc>
 
-CONFIG  FOSC = INTOSC         ; INTOSC oscillator
-CONFIG  WDTE = OFF            ; Watchdog Timer disabled
-CONFIG  PWRTE = ON            ; Power-up Timer enabled
-CONFIG  MCLRE = ON            ; MCLR/VPP pin function is MCLR
-CONFIG  CP = OFF              ; Flash Program Memory Code Protection off
-CONFIG  CPD = OFF             ; Data Memory Code Protection off
-CONFIG  BOREN = ON            ; Brown-out Reset enabled
-CONFIG  CLKOUTEN = OFF        ; Clock Out disabled
-CONFIG  IESO = ON             ; Internal/External Switchover enabled
-CONFIG  FCMEN = ON            ; Fail-Safe Clock Monitor enabled
-CONFIG  WRT = OFF             ; Flash Memory Self-Write Protection off
-CONFIG  VCAPEN = OFF          ; Voltage Regulator Capacitor disabled
-;CONFIG  PLLEN = ON            ; 4x PLL enabled
-CONFIG  STVREN = ON           ; Stack Overflow/Underflow Reset enabled
-CONFIG  BORV = LO             ; Brown-out Reset Voltage trip point low
-CONFIG  LPBOR = OFF           ; Low Power Brown-Out Reset disabled
-CONFIG  LVP = OFF             ; Low-Voltage Programming disabled
+CONFIG  FOSC = INTOSC   ; INTOSC oscillator
+CONFIG  WDTE = OFF      ; Watchdog Timer disabled
+CONFIG  PWRTE = ON      ; Power-up Timer enabled
+CONFIG  MCLRE = ON      ; MCLR/VPP pin function is MCLR
+CONFIG  CP = OFF        ; Flash Program Memory Code Protection off
+CONFIG  CPD = OFF       ; Data Memory Code Protection off
+CONFIG  BOREN = ON      ; Brown-out Reset enabled
+CONFIG  CLKOUTEN = OFF  ; Clock Out disabled
+CONFIG  IESO = ON       ; Internal/External Switchover enabled
+CONFIG  FCMEN = ON      ; Fail-Safe Clock Monitor enabled
+CONFIG  WRT = OFF       ; Flash Memory Self-Write Protection off
+CONFIG  VCAPEN = OFF    ; Voltage Regulator Capacitor disabled
+CONFIG  STVREN = ON     ; Stack Overflow/Underflow Reset enabled
+CONFIG  BORV = LO       ; Brown-out Reset Voltage trip point low
+CONFIG  LPBOR = OFF     ; Low Power Brown-Out Reset disabled
+CONFIG  LVP = OFF       ; Low-Voltage Programming disabled
 
 PSECT udata_shr
 ready:                  ; semaphore used to know if the timer interrupt has occured
@@ -55,9 +54,9 @@ servoh:                 ; horizontal servo
         DS      1
 servov:                 ; vertical servo
         DS      1
-temp:                   ; temporaty register
+temp:                   ; temporary register
         DS      1
-counter:
+counter:                ; interrupt counter th slow the system a bit down
         DS      1
 
 PSECT reset_vec, class = CODE, delta = 2  
@@ -74,57 +73,60 @@ start:  call    init_clock      ; 2MHz oscillator initialization
         call    init_portb      ; PORTB initialisation for led
         call    init_data
         call    init_timer_interrupt
-        call    init_pwm
+        call    init_pwm        ; PWM mode initialisation for servos
         clrf    BSR
-        goto    loop            ; enter event loop
+        goto    loop
 
-; Initialization
+; Initialise FOSC
 init_clock:
         banksel OSCCON
         movlw   0x60            ; 2MHz HF, FOSC bits in config
         movwf   OSCCON
         return
 
+; Initialise ADC module for ldr voltage capture
 init_adc:
-        banksel PORTA           ; PORTA initialization
+        banksel PORTA   ; PORTA initialization
         clrf    PORTA
         banksel LATA
         clrf    LATA
         banksel TRISA
-        clrf    TRISA           ; set RA<0:3> to input
+        clrf    TRISA   ; set RA<0:3> to input
         banksel ANSELA
         movlw   0xff
-        movwf   ANSELA          ; set RA<0:3> to analog
+        movwf   ANSELA  ; set RA<0:3> to analog
         banksel WPUA
         movlw   0x00
-        movwf   WPUE            ; weak pull-ups disabled
+        movwf   WPUE    ; weak pull-ups disabled
         banksel ADCON2
         movlw   0x0f
-        movwf   ADCON2          ; CHSN: single-ended signal
+        movwf   ADCON2  ; CHSN: single-ended signal
         movlw   0xf0
-        movwf   ADCON1          ; Reference setting + FRC clock (see p.171)
+        movwf   ADCON1  ; Reference setting + FRC clock (see p.171)
         return
 
+; Initialise port b for led blinking
 init_portb:
-        banksel PORTB
+        banksel PORTB   ; PORTB initialization
         clrf    PORTB
         banksel LATB
         clrf    LATB
         banksel ANSELB
-        clrf    ANSELB
+        clrf    ANSELB  ; set RB<0 to digital
         banksel TRISB
-        clrf    TRISB
+        clrf    TRISB   ; set RB0 to output
         return
 
 init_data:
-        clrf    ready           ; clear ready flag
+        clrf    ready   ; clear ready flag
         movlw   0x30            
-        movwf   servoh          ; initialize 360 servo to a centered position
+        movwf   servoh  ; initialize 360 servo to a centered position
         movlw   0x30
-        movwf   servov          ; initialize 180 servo to a centered position
+        movwf   servov  ; initialize 180 servo to a centered position
         clrf    counter
         return
 
+; Initialise the timer 0 for interrupts
 init_timer_interrupt:
         bsf     OPTION_REG, 1
         bsf     OPTION_REG, 2
@@ -134,6 +136,7 @@ init_timer_interrupt:
         bsf     INTCON, 7
         return
 
+; Setup for pwm operation
 init_pwm:
         banksel PORTC
         clrf    PORTC
@@ -141,10 +144,10 @@ init_pwm:
         clrf    LATC
         banksel TRISC
         movlw   0xff
-        movf    TRISC           ; set RC0->RC7 to output
+        movf    TRISC   ; set RC<1:2> to output
         banksel PR2
         movlw   0x9b
-        movwf   PR2             ; pwm period (p.228) of 20ms
+        movwf   PR2     ; pwm period (p.228) of 20ms
         ; 360 servo
         banksel CCP1CON
         bsf     CCP1CON, 2      ; pwm mode
@@ -185,7 +188,7 @@ init_pwm:
         banksel CCP2CON
         iorwf   CCP2CON, 1
         ; configure and start time 2
-        banksel PIR1            ; timer 2 init and start
+        banksel PIR1    ; timer 2 init and start
         bcf     PIR1, 1
         bsf     T2CON, 0        ; prescaler de 1:64
         bsf     T2CON, 1
@@ -194,26 +197,27 @@ init_pwm:
         clrf    TRISC
         return
 
-; main code
+; main loop
 loop:   
-        btfsc   ready, 0        ; Check ready every XX seconds
-	call    computation     ; computations for LDRs
+        btfsc   ready, 0        ; Check ready. If set -> enter, skip instruction otherwise
+	call    computation
         goto    loop
 
+; Compute new system state
 computation:
         clrf    ready
         ; Horizontal (360) servomotor
-        call    ldr0            ; Get LDR value (down)
-        call    ldr1            ; Get LDR value (up)
-        call    differenceH_360    ; Get vertical servomotor's direction
+        call    ldr0    ; Get LDR value (down)
+        call    ldr1    ; Get LDR value (up)
+        call    differenceH_360 ; Get vertical servomotor's direction
         ; Vertical (180) servomotor
-        call    ldr2            ; Get LDR value (left)
-        call    ldr3            ; Get LDR value (right)
-        call    differenceH_180    ; Get horizontal servomotor's direction
+        call    ldr2    ; Get LDR value (left)
+        call    ldr3    ; Get LDR value (right)
+        call    differenceH_180 ; Get horizontal servomotor's direction
         call    pwm
         return
 
-; Getters
+; Compute ldr values
 ldr0:
         banksel ADCON0
         movlw   0x81
@@ -282,36 +286,83 @@ ldr3:
         bcf     ldr3l, 1
         return
 
-; Movers
-turn_right_360:
-        banksel PORTB
-        movlw   0xff
-        movwf   PORTB
-        movlw   0x2d
-        movwf   servoh
+; Call servo turners according to ldr values
+differenceH_360:
+        movf    ldr0h, 0
+        subwf   ldr1h
+        btfsc   STATUS, 2       ; enter if Z = 1 (equal)
+        goto    differenceL_360 ; Call differenceL_360 if equal
+        btfsc   STATUS, 0       ; enter if ldr0 > ldr1
+        goto    turn_left_360
+        goto    turn_right_360
+        return
+        
+differenceL_360:
+        movf    ldr0l, 0
+        subwf   ldr1l
+        btfsc   STATUS, 2       ; enter if Z = 1 (equal)
+        goto    stop
+        btfsc   STATUS, 0       ; enter if ldr0 > ldr1s
+        goto    turn_left_360
+        goto    turn_right_360
         return
 
+differenceH_180:
+        movf    ldr2h, 0
+        subwf   ldr3h
+        btfsc   STATUS, 2       ; enter if Z = 1 (equal)
+        goto    differenceL_180 ; Call differenceL_360 if equal
+        btfsc   STATUS, 0       ; enter if ldr0 > ldr1
+        goto    turn_left_180
+        goto    turn_right_180
+        return
+        
+differenceL_180:
+        movf    ldr2l, 0
+        subwf   ldr3l
+        btfsc   STATUS, 2       ; enter if Z = 1 (equal)
+        return
+        btfsc   STATUS, 0       ; enter if ldr0 > ldr1s
+        goto    turn_left_180
+        goto    turn_right_180
+        return
+
+; Compute horizontal servo new position
 turn_left_360:
         banksel PORTB
         movlw   0x00
-        movwf   PORTB
+        movwf   PORTB   ; set led off
         movlw   0x30
+        movwf   servoh
+        return
+
+turn_right_360:
+        banksel PORTB
+        movlw   0xff
+        movwf   PORTB   ; set led on
+        movlw   0x2d
         movwf   servoh
         return
 
 stop:
         banksel PORTB
         movlw   0x00
-        movwf   PORTB
+        movwf   PORTB   ; set led off
         movlw   0x2f
         movwf   servoh
         return
 
+; compute vertical servo new position
+turn_left_180:
+        movlw   0x50    ; far left
+        subwf   servov, 0
+        btfsc   STATUS, 0
+        return
+        incf    servov, 1
+        return
+
 turn_right_180:
-        ;banksel PORTB
-        ;movlw   0xff
-        ;movwf   PORTB
-        movlw   0x10
+        movlw   0x10    ; far right
         movwf   temp
         movf    servov, 0
         subwf   temp, 0
@@ -320,17 +371,7 @@ turn_right_180:
         decf    servov, 1
         return
 
-turn_left_180:
-        ;banksel PORTB
-        ;movlw   0x00
-        ;movwf   PORTB
-        movlw   0x50
-        subwf   servov, 0
-        btfsc   STATUS, 0
-        return
-        incf    servov, 1
-        return
-
+; Move both servos according to new positions calculated
 pwm:
         ; 360 servo
         movf    servoh, 0
@@ -368,47 +409,7 @@ pwm:
         iorwf   CCP2CON, 1
         return
 
-; Utility functions
-differenceH_360:
-        movf    ldr0h, 0
-        subwf   ldr1h
-        btfsc   STATUS, 2       ; enter if Z = 1 (equal)
-        goto    differenceL_360    ; Call differenceL_360 if equal
-        btfsc   STATUS, 0       ; enter if ldr0 > ldr1
-        goto    turn_left_360
-        goto    turn_right_360
-        return
-        
-differenceL_360:
-        movf    ldr0l, 0
-        subwf   ldr1l
-        btfsc   STATUS, 2       ; enter if Z = 1 (equal)
-        goto    stop
-        btfsc   STATUS, 0       ; enter if ldr0 > ldr1s
-        goto    turn_left_360
-        goto    turn_right_360
-        return
-
-differenceH_180:
-        movf    ldr2h, 0
-        subwf   ldr3h
-        btfsc   STATUS, 2       ; enter if Z = 1 (equal)
-        goto    differenceL_180    ; Call differenceL_360 if equal
-        btfsc   STATUS, 0       ; enter if ldr0 > ldr1
-        goto    turn_left_180
-        goto    turn_right_180
-        return
-        
-differenceL_180:
-        movf    ldr2l, 0
-        subwf   ldr3l
-        btfsc   STATUS, 2       ; enter if Z = 1 (equal)
-        return
-        btfsc   STATUS, 0       ; enter if ldr0 > ldr1s
-        goto    turn_left_180
-        goto    turn_right_180
-        return
-
+; Acquisition delay after ADC channel switch
 delay:
         movlw   0xff
         movwf   delay_h
